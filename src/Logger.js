4b5263715708065e261405ce983ade7cfe77c1ff
Yevhen Fabizhevskyi <fabasoad@gmail.com>
2020-03-27 20:34:41 +0900

    Trigger build

--- src/Logger.js
+++ src/Logger.js
@@ -2,7 +2,6 @@ const { createLogger, format, transports } = require('winston');
 const { combine, timestamp, label, printf } = format;
 
 class Logger {
-
   constructor(clazz) {
     const customFormat = printf(({ level, message, label, timestamp }) => {
       timestamp = timestamp.replace(/T/, ' ').replace(/\..+/, '');
