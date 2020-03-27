9fc607911645863aead05ddaa97de7739399b5cd
Yevhen Fabizhevskyi <fabasoad@gmail.com>
2020-03-27 19:11:15 +0900

    Fix runtime error

--- src/Installer.js
+++ src/Installer.js
@@ -20,7 +20,7 @@ class Installer {
     const oldPath = await tc.downloadTool(this.getUrl());
     this.logger.info(`Downloaded to ${oldPath}.`);
     const newPath = path.join(path.basename(path.dirname(oldPath)), this.EXEC_FILE);
-    fs.renameSync(downloadedFile, newPath);
+    fs.renameSync(oldPath, newPath);
     this.logger.info(`Renamed to ${newPath}.`);
     
     const cachedPath = await tc.cacheDir('.', this.EXEC_FILE, this.version);
