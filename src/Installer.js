517c2c493745427455061dfbfee2cc4a39319e61
Yevhen Fabizhevskyi <fabasoad@gmail.com>
2020-03-27 18:49:49 +0900

    Fix rename function

--- src/Installer.js
+++ src/Installer.js
@@ -18,7 +18,7 @@ class Installer {
     this.logger.info(`Downloading Mint ${this.version}...`);
     const downloadedFile = await tc.downloadTool(this.getUrl());
     this.logger.info(`Installing Mint ${this.version}...`);
-    await fs.rename(downloadedFile, this.EXEC_FILE);
+    fs.renameSync(downloadedFile, this.EXEC_FILE);
     
     const cachedPath = await tc.cacheDir('.', this.EXEC_FILE, this.version);
     core.addPath(cachedPath);
