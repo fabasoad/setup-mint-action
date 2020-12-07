31822f4f55f1db249db391497c833b77dbf1b34c
fabasoad <fabasoad@gmail.com>
2020-12-07 22:01:44 +0900

    Add linter

--- src/installer.js
+++ src/installer.js
@@ -13,7 +13,6 @@ class UnsupportedOSError extends Error {
 }
 
 class Installer {
-
   constructor(version) {
     this.version = version;
     this.logger = new Logger('Installer');
@@ -25,15 +24,16 @@ class Installer {
     this.logger.info(`Downloading Mint ${this.version}...`);
     const oldPath = await tc.downloadTool(this.getUrl());
     this.logger.info(`Downloaded to ${oldPath}.`);
-    const index = oldPath.lastIndexOf(path.sep);    
+    const index = oldPath.lastIndexOf(path.sep);
     const folderPath = oldPath.substring(0, index);
     const newPath = path.join(folderPath, this.EXEC_FILE);
     fs.renameSync(oldPath, newPath);
     this.logger.info(`Renamed to ${newPath}.`);
     fs.chmodSync(newPath, '777');
     this.logger.info(`Access permissions changed to 777.`);
-    
-    const cachedPath = await tc.cacheDir(folderPath, this.EXEC_FILE, this.version);
+
+    const cachedPath =
+      await tc.cacheDir(folderPath, this.EXEC_FILE, this.version);
     this.logger.info(`Cached dir is ${cachedPath}`);
     core.addPath(cachedPath);
   }
@@ -41,14 +41,14 @@ class Installer {
   getUrl() {
     let suffix;
     switch (os.type()) {
-      case 'Darwin':
-        suffix = 'osx';
-        break;
-      case 'Linux':
-        suffix = 'linux';
-        break;
-      default:
-        throw new UnsupportedOSError('Windows is not supported.');
+    case 'Darwin':
+      suffix = 'osx';
+      break;
+    case 'Linux':
+      suffix = 'linux';
+      break;
+    default:
+      throw new UnsupportedOSError('Windows is not supported.');
     }
     return `https://github.com/mint-lang/mint/releases/download/${this.version}/mint-${this.version}-${suffix}`;
   }
