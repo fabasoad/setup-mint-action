679c49cd2968bfb67038443351681f4fe83e40bb
fabasoad <fabasoad@gmail.com>
2024-11-27 06:14:25 +0900

    Fix binary name

--- src/collect-info.sh
+++ src/collect-info.sh
@@ -2,8 +2,9 @@
 
 # Starting from v0.20.0-alpha.1 mint supports macos arm64 OS. This function
 # checks if requested version is less than 0.20.x
-if_old_version() {
-  version="$1"
+_if_old_version() {
+  version="${1}"
+
   major=$(echo "$version" | cut -d '.' -f 1)
   minor=$(echo "$version" | cut -d '.' -f 2)
   if [ "$major" -lt 1 ] && [ "$minor" -lt 20 ]; then
@@ -14,30 +15,32 @@ if_old_version() {
 }
 
 main() {
+  input_version="${1}"
+
   echo "mint-installed=$(if command -v mint >/dev/null 2>&1; then echo true; else echo false; fi)" >> "$GITHUB_OUTPUT"
   mkdir -p "${GITHUB_WORKSPACE}/mint"
   echo "mint-path=${GITHUB_WORKSPACE}/mint" >> "$GITHUB_OUTPUT"
   if [ "${RUNNER_OS}" = "Linux" ]; then
-    MINT_BINARY=mint-${INPUT_VERSION}-linux
+    mint_binary="mint-${input_version}-linux"
   else
     if [ "${RUNNER_ARCH#ARM}" != "$RUNNER_ARCH" ]; then
-      if [ "$(if_old_version "${INPUT_VERSION}")" = "true" ]; then
-        msg="${RUNNER_OS} ${RUNNER_ARCH} is not supported by mint ${INPUT_VERSION}."
-        msg="${msg} Try newer version of mint (> 0.19.x)."
+      if [ "$(_if_old_version "${input_version}")" = "true" ]; then
+        msg="${RUNNER_OS} ${RUNNER_ARCH} is not supported by mint ${input_version}."
+        msg="${msg} Try newer version of mint (>= 0.20.0)."
         echo "::error title=OS is not supported::${msg}"
         exit 1
       else
-        MINT_BINARY=mint-${INPUT_VERSION}-macos-latest
+        mint_binary="mint-${input_version}-osx-arm64"
       fi
     else
-      if [ "$(if_old_version "${INPUT_VERSION}")" = "true" ]; then
-        MINT_BINARY=mint-${INPUT_VERSION}-osx
+      if [ "$(_if_old_version "${input_version}")" = "true" ]; then
+        MINT_BINARY="mint-${input_version}-osx"
       else
-        MINT_BINARY=mint-${INPUT_VERSION}-macos-13
+        MINT_BINARY="mint-${input_version}-osx-x86_64"
       fi
     fi
   fi
-  echo "mint-binary=$MINT_BINARY" >> "$GITHUB_OUTPUT"
+  echo "mint-binary=${mint_binary}" >> "$GITHUB_OUTPUT"
 }
 
 main "$@"
